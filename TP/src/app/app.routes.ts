5de3a6e02bff92401fdcf8969110186f3511a9a0
Tony-UY <uy.tony@outlook.fr>
2025-02-18 16:49:40 +0100

    Etape 1/2 fait

--- TP/src/app/app.routes.ts
+++ TP/src/app/app.routes.ts
@@ -1,5 +1,32 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {Page404Component} from "./page404/page404.component";
+import {DetailvoyageComponent} from "./detailvoyage/detailvoyage.component";
+import {GenVoyageComponent} from "./gen-voyage/gen-voyage.component";
 
-export const routes: Routes = [];
-
-
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'home',
+    redirectTo: '',
+  },
+  {
+    path: 'gen',
+    component: GenVoyageComponent,
+  },
+  {
+    path: 'detail/:id',
+    component: DetailvoyageComponent,
+  },
+  {
+    path: '404',
+    component: Page404Component,
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+  },
+];
---
+++ TP/src/app/models/Voyage.ts
@@ -0,0 +1,13 @@
+class Voyage {
+  id: number;
+  nom: string;
+  description: string;
+  price: number;
+  durée
+  constructor(message: string) {
+    this.greeting = message;
+  }
+
+
+}
+
